de65da4f34ce618f845bab66a706e976aeafdd36
brunogomescarvalho <7bruno8@gmail.com>
2023-02-18 10:08:29 -0300

    first commit

---
+++ Desafio01/Program.cs
@@ -0,0 +1,35 @@
+﻿/*ESCREVA UM PROGRAMA QUE CONTE A QUANTIDADE DE PALAVRAS 
+E CARAVTERES DE UM DETERMINADO TEXTO, ESCRITO NO CONSOLE*/
+
+string frase;
+do
+{
+    Console.Clear();
+    Console.WriteLine("Digite sua frase:   ('Digite sair para encerrar.')\n");
+    frase = Console.ReadLine()!;
+  
+    if (frase == "sair") return;
+
+    string[] palavras = frase.Split(' ');
+    frase.Replace(" ", "");
+
+    Console.WriteLine($"{palavras.Length} Palvaras.\n{frase.Length} Caracteres.");
+    Console.WriteLine("\nTecle para continuar");
+    Console.ReadKey();
+
+} while (frase != "sair");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
