dc43f15bf4fe0a2d4fab60e457e35d2adcf79e5a
brunogomescarvalho <7bruno8@gmail.com>
2023-02-20 19:41:53 -0300

    segundocommit

--- Desafio04/Program.cs
+++ Desafio04/Program.cs
@@ -15,127 +15,67 @@ double valorCompra = Double.Parse(Console.ReadLine()!);
 Console.WriteLine("Digite o valor Pago");
 double valorPago = Double.Parse(Console.ReadLine()!);
 
-double troco = Math.Round(valorPago - valorCompra, 2);
+double trocoCalculado = Math.Round(valorPago - valorCompra, 2);
 
-int notaDeCem = 0;
-int notaDeCinquenta = 0;
-int notaDeVinte = 0;
-int notaDeDez = 0;
-int notaDeCinco = 0;
-int notaDeDois = 0;
+System.Console.WriteLine($"Troco: {trocoCalculado}");
 
-Console.Clear();
-System.Console.WriteLine($"Troco Total: R$ {troco}\n");
+double[] valores = { 100, 50, 20, 10, 5, 2, 1, .50, .25, .10, .5, .1 };
 
-do
+List<Troco> valoresNoTroco = new List<Troco>();
+
+foreach (var item in valores)
 {
-    if (troco >= 100)
+    Troco trocoAPagar = new Troco(0, 0);
+
+    while (trocoCalculado >= item)
     {
-        notaDeCem++;
-        troco -= 100;
+        bool novaCedula = valoresNoTroco.Find(i => i.cedula == item) == null;
+        if (trocoCalculado >= item)
+        {
+
+            if (novaCedula)
+            {
+                trocoAPagar.quantidade++;
+                trocoAPagar.cedula = item;
+
+                valoresNoTroco.Add(trocoAPagar);
+                trocoCalculado -= item;
+            }
+            else
+            {
+                foreach (var i in valoresNoTroco)
+                {
+                    if (i.cedula == item)
+                    {
+                        i.quantidade++;
+                        trocoCalculado -= item;
+
+                    }
+                }
+            }
+        }
     }
-    if (troco >= 50)
-    {
-        notaDeCinquenta++;
-        troco -= 50;
-    }
-    if (troco >= 20)
-    {
-        notaDeVinte++;
-        troco -= 20;
-    }
-    if (troco >= 10)
-    {
-        notaDeDez++;
-        troco -= 10;
-    }
-    if (troco >= 5)
-    {
-        notaDeCinco++;
-        troco -= 5;
-    }
-    if (troco >= 2)
-    {
-        notaDeDois++;
-        troco -= 2;
-    }
+}
 
-} while (troco > 2);
-
-Console.WriteLine("Cédulas\n");
-Console.WriteLine("R$100 - R$50 ----- R$20 ---- R$10 ----- R$5 ----- R$2");
-
-Console.Write(notaDeCem);
-Console.CursorLeft = 10;
-
-System.Console.Write(notaDeCinquenta);
-Console.CursorLeft = 20;
-
-System.Console.Write(notaDeVinte);
-Console.CursorLeft = 30;
-
-System.Console.Write(notaDeDez);
-Console.CursorLeft = 40;
-
-System.Console.Write(notaDeCinco);
-Console.CursorLeft = 50;
-
-System.Console.Write(notaDeDois);
-
-Console.WriteLine("\n\nMoedas\n");
-Console.WriteLine("R$1 - R$0,50 ----- R$0,25 ---- R$0,10 ----- R$0,5");
-
-int moedaDeUmReal = 0;
-int moedaDe50 = 0;
-int moedaDe25 = 0;
-int moedaDe10 = 0;
-int moedaDe5 = 0;
-
-do
+foreach (var item in valoresNoTroco)
 {
-    if (troco >= 1)
+    System.Console.WriteLine($"{item}");
+}
+return;
+
+public class Troco
+{
+    public int quantidade;
+    public double cedula;
+
+    public Troco(int qtd, int cedula)
     {
-        moedaDeUmReal++;
-        troco -= 1;
-    }
-    if (troco >= .50)
-    {
-        moedaDe50++;
-        troco -= .50;
-    }
-    if (troco >= .25)
-    {
-        moedaDe25++;
-        troco -= .25;
-    }
-    if (troco >= .10)
-    {
-        moedaDe10++;
-        troco -= .10;
-    }
-    if (troco >= .5)
-    {
-        moedaDe5++;
-        troco -= .5;
+        this.cedula = cedula;
+        this.quantidade = qtd;
     }
 
-} while (troco > .5);
-
-
-Console.Write(moedaDeUmReal);
-Console.CursorLeft = 10;
-
-System.Console.Write(moedaDe50);
-Console.CursorLeft = 23;
-
-System.Console.Write(moedaDe25);
-Console.CursorLeft = 33;
-
-System.Console.Write(moedaDe10);
-Console.CursorLeft = 45;
-
-System.Console.Write($"{moedaDe5}\n\n");
-
-
-
-
+    public override string ToString()
+    {
+        return $" {quantidade} {(cedula > 1 ? "Cédula" : "Moeda")} de R$ {cedula}";
+    }
+}
