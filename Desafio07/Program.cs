f726101532bdde40f7f5c1ad89ea4348be812f87
brunogomescarvalho <7bruno8@gmail.com>
2023-02-23 21:34:38 -0300

    first commit

---
+++ Desafio07/Program.cs
@@ -0,0 +1,29 @@
+﻿var inicio = 0;
+var proximo = 1;
+int sequencia = 0;
+string piramide = "0 ";
+
+
+for (int i = 0; i < 20; i++)
+{
+    System.Console.WriteLine(piramide);
+
+    if (inicio + sequencia == 1)
+    {
+        piramide += proximo.ToString() + " ";
+        System.Console.WriteLine(piramide);
+    }
+
+    sequencia = inicio;
+    inicio = proximo;
+    proximo = inicio + sequencia;
+    piramide += proximo.ToString() + " ";
+
+}
+
+
+
+
+
+
+
